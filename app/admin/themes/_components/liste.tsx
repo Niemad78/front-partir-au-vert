dbc81695735117169b81bd07895101efc555d7d7
Damien Mauger <mauger.damien@hotmail.fr>
2025-12-07 03:25:25 +0100

    feat: ajout d'un filtre pour chercher un thème

--- app/admin/themes/_components/liste.tsx
+++ app/admin/themes/_components/liste.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { ConfirmDialog } from "primereact/confirmdialog";
 import { SuppressionTheme } from "./suppressionTheme";
 import { Image } from "@/lib/api/type";
 import { ImageNext } from "@/components/image";
 import * as Table from "@/components/table";
+import { useState } from "react";
+import { FloatLabel } from "primereact/floatlabel";
+import { InputText } from "primereact/inputtext";
 
 type ListeThemesProps = {
   themes: {
@@ -15,9 +17,22 @@ type ListeThemesProps = {
 };
 
 export function ListeThemes({ themes }: ListeThemesProps) {
+  const [filtre, setFiltre] = useState("");
+
   return (
     <>
-      <ConfirmDialog />
+      <div className="my-[20px] w-[500px]">
+        <FloatLabel>
+          <InputText
+            id="filtre"
+            name="filtre"
+            value={filtre}
+            onChange={(e) => setFiltre(e.target.value)}
+            className="w-full"
+          />
+          <label htmlFor="filtre">Rechercher</label>
+        </FloatLabel>
+      </div>
 
       <Table.Table>
         <Table.Header>
@@ -28,24 +43,37 @@ export function ListeThemes({ themes }: ListeThemesProps) {
           <Table.Cell className="border-none">Actions</Table.Cell>
         </Table.Header>
         <Table.Rows>
-          {themes.map((theme) => (
-            <Table.Row key={theme.id}>
-              <Table.Cell className="text-left">{theme.nom}</Table.Cell>
-              <Table.Cell>
-                {theme.image && (
-                  <ImageNext
-                    src={theme.image.nom}
-                    alt={theme.nom}
-                    width={60}
-                    height={60}
-                  />
-                )}
-              </Table.Cell>
-              <Table.Cell>
-                <SuppressionTheme themeId={theme.id} />
-              </Table.Cell>
-            </Table.Row>
-          ))}
+          {themes
+            .filter((theme) =>
+              theme.nom
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .includes(
+                  filtre
+                    .toLowerCase()
+                    .normalize("NFD")
+                    .replace(/[\u0300-\u036f]/g, ""),
+                ),
+            )
+            .map((theme) => (
+              <Table.Row key={theme.id}>
+                <Table.Cell className="text-left">{theme.nom}</Table.Cell>
+                <Table.Cell>
+                  {theme.image && (
+                    <ImageNext
+                      src={theme.image.nom}
+                      alt={theme.nom}
+                      width={60}
+                      height={60}
+                    />
+                  )}
+                </Table.Cell>
+                <Table.Cell>
+                  <SuppressionTheme themeId={theme.id} />
+                </Table.Cell>
+              </Table.Row>
+            ))}
         </Table.Rows>
       </Table.Table>
     </>
