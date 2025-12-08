ca03523af9beb5f721dc81afbf94e0dd2eeab43f
Damien Mauger <mauger.damien@hotmail.fr>
2025-12-08 21:34:23 +0100

    feat: modification d'un theme

--- lib/api/resources/theme.ts
+++ lib/api/resources/theme.ts
@@ -1,11 +1,10 @@
-import { DELETE, GET, POST } from "../client";
+import { DELETE, GET, POST, PUT } from "../client";
 import { BaseResult, Theme } from "../type";
 
 type ThemeListe = BaseResult & {
   themes: {
     id: string;
     nom: string;
-    description: string;
   }[];
 };
 
@@ -33,6 +32,34 @@ export async function getThemes(token: string) {
   };
 }
 
+type ThemeById = BaseResult & {
+  theme: Theme;
+};
+
+export async function getThemeById(token: string, id: string) {
+  const response = await GET<ThemeById>(`/themes/${id}`, {
+    credentials: "include",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Cookie: `session=${token}`,
+    },
+    cache: "no-store",
+  });
+
+  if (!response.ok) {
+    return {
+      ok: response.ok,
+      status: response.status ?? 500,
+      errorMessage: response.errorMessage ?? "Une erreur est survenue",
+    };
+  }
+
+  return {
+    ok: response.ok,
+    data: response.theme,
+  };
+}
+
 type NouveauTheme = {
   data: Theme;
   token?: string;
@@ -50,6 +77,37 @@ export async function nouveauTheme({
     },
     cache: "no-store",
   });
+
+  if (!response.ok) {
+    return {
+      ok: response.ok,
+      status: response.status ?? 500,
+      errorMessage: response.errorMessage ?? "Une erreur est survenue",
+    };
+  }
+
+  return {
+    ok: response.ok,
+    message: response.message,
+  };
+}
+
+export async function modifierTheme({
+  data,
+  token,
+}: NouveauTheme): Promise<BaseResult> {
+  const response = await PUT<BaseResult, Theme>(
+    `/themes/modification/${data.id}`,
+    data,
+    {
+      credentials: "include",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Cookie: `session=${token}`,
+      },
+      cache: "no-store",
+    },
+  );
 
   if (!response.ok) {
     return {
