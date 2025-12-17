b3040d88b9f5899a9fffbac6da823900631ec923
Damien Mauger <mauger.damien@hotmail.fr>
2025-12-17 18:59:55 +0100

    feat: création et initialisation breadcrumb

--- app/admin/mes-informations/page.tsx
+++ app/admin/mes-informations/page.tsx
@@ -1,9 +1,11 @@
+import Breadcrumb from "@/components/breadcrumb";
 import MesInformationsForm from "./_components/form";
 
 export default function Page() {
   return (
     <section>
-      <h1 className="text-primary">Mes informations</h1>
+      <h1 className="text-primary mb-[30px]">Mes informations</h1>
+      <Breadcrumb items={[{ label: "Mes informations" }]} />
       <div className="mt-[100px] flex w-full flex-col items-center justify-center gap-y-[30px]">
         <p>Modifiez vos informations personnelles</p>
         <MesInformationsForm />
