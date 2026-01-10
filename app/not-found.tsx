1bc73e23820da707cbc727ec7fade7006081ed72
Damien Mauger <mauger.damien@hotmail.fr>
2026-01-11 00:48:48 +0100

    feat: création de la page 404

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="background-campagne flex h-[100vh] w-[100vw] flex-col items-center justify-center">
+      <div className="background text-primary rounded-md bg-white/80 p-[40px] text-center">
+        <h3>404</h3>
+        <p>Désolé, la page que vous recherchez est introuvable.</p>
+        <Link href="/">Retour à l&#39;accueil</Link>
+      </div>
+    </div>
+  );
+}
