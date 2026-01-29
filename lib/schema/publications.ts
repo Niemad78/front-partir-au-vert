66f5fc3c4d8ded2edae5f2cd34be15b1ac8bcee5
Damien Mauger <mauger.damien@hotmail.fr>
2026-01-29 22:06:48 +0100

    change: ajout de la publication mentions légales

--- lib/schema/publications.ts
+++ lib/schema/publications.ts
@@ -4,6 +4,6 @@ export const NouvellePublicationSchema = Yup.object().shape({
   titre: Yup.string().required("Titre requis"),
   contenu: Yup.string().required("Contenu requis"),
   type: Yup.string()
-    .oneOf(["histoire", "seminaire", "autre"])
+    .oneOf(["histoire", "seminaire", "autre", "mentions_legales"])
     .required("Type requis"),
 });
