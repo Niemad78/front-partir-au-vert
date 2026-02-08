c5f1774b67ee9d2583b079403922d9986beceab3
Damien Mauger <mauger.damien@hotmail.fr>
2026-02-08 16:39:09 +0100

    feat: filtre version mobile

--- app/(public)/_components/filtreDesktop.tsx
+++ app/(public)/_components/filtreDesktop.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Duree, DureeKey } from "@/lib/api/type";
 import { getThemes } from "@/lib/api/resources/theme";
 import { Dropdown } from "primereact/dropdown";
 import { useEffect, useState } from "react";
@@ -12,13 +11,13 @@ import {
   setPrix,
   setNbPersonnes,
 } from "@/lib/store/filtreSlice";
+import {
+  dureeOptions,
+  personnesOptions,
+  prixOptions,
+} from "@/lib/filtreOptions";
 
-const dureeOptions = Object.entries(Duree).map(([key, label]) => ({
-  label,
-  value: key as DureeKey,
-}));
-
-export default function Filtre() {
+export default function FiltreDesktop() {
   const dispatch = useAppDispatch();
   const { themeChoisi, dureeChoisie, prix, nbPersonnes } = useAppSelector(
     (state) => state.filtre,
@@ -26,38 +25,6 @@ export default function Filtre() {
   const [themeOptions, setThemeOptions] = useState<
     { label: string; value: string }[]
   >([]);
-
-  const personnesOptions = [
-    { label: "1 personne", value: 1 },
-    { label: "2 personnes", value: 2 },
-    { label: "3 personnes", value: 3 },
-    { label: "4 personnes", value: 4 },
-    { label: "5 personnes", value: 5 },
-    { label: "6 personnes", value: 6 },
-    { label: "7 personnes", value: 7 },
-    { label: "8 personnes", value: 8 },
-    { label: "9 personnes", value: 9 },
-    { label: "10 personnes", value: 10 },
-    { label: "11 personnes", value: 11 },
-    { label: "12 personnes", value: 12 },
-    { label: "13 personnes", value: 13 },
-    { label: "14 personnes", value: 14 },
-    { label: "15 personnes", value: 15 },
-    { label: "16 personnes", value: 16 },
-    { label: "17 personnes", value: 17 },
-    { label: "18 personnes", value: 18 },
-    { label: "19 personnes", value: 19 },
-    { label: "20 personnes", value: 20 },
-    { label: "Plus de 20 personnes", value: 21 },
-  ];
-
-  const prixOptions = [
-    { label: "Moins de 50€", value: 50 },
-    { label: "50€ - 100€", value: 100 },
-    { label: "100€ - 150€", value: 150 },
-    { label: "150€ - 200€", value: 200 },
-    { label: "Plus de 200€", value: 201 },
-  ];
 
   useEffect(() => {
     getThemes().then((themesData) => {
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,17 +1,23 @@
-import Filtre from "./_components/filtre";
+import FiltreDesktop from "./_components/filtreDesktop";
+import FiltreMobile from "./_components/filtreMobile";
 
 export default function Home() {
   return (
     <section>
-      <div className="background-accueil flex h-[500px] w-full flex-col items-center justify-end p-[50px]">
+      <div className="background-accueil flex h-[500px] w-full flex-col items-center justify-end p-[50px] text-center">
         <h2 className="text-white">
           Partir en excursion... À moins de 1 heure de Paris !
         </h2>
         <h3 className="text-white">
-          Faites le plein de découvertes culturelles et gourmande au cours d’une
-          journée exceptionnelle
+          Faites le plein de découvertes culturelles et gourmande au cours
+          d&#39;une journée exceptionnelle
         </h3>
-        <Filtre />
+        <div className="hidden lg:block">
+          <FiltreDesktop />
+        </div>
+        <div className="block lg:hidden">
+          <FiltreMobile />
+        </div>
       </div>
     </section>
   );
