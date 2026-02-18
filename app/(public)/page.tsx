94cdbd124801b626b265cea05a80b0e1fdf58730
Damien Mauger <mauger.damien@hotmail.fr>
2026-02-18 22:39:31 +0100

    feat: accordéon faq

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,10 +2,14 @@ import { getActivites } from "@/lib/api/resources/activite/activite";
 import FiltreDesktop from "./_components/filtreDesktop";
 import FiltreMobile from "./_components/filtreMobile";
 import CarteActivite from "./_components/carteActivite";
+import { getFaq } from "@/lib/api/resources/faq/faq";
+import Accordeon from "./_components/accordeon";
 
 export default async function Home() {
   const activiteData = await getActivites();
   const activites = activiteData.data ?? [];
+  const faqData = await getFaq();
+  const faqs = faqData.data ?? [];
 
   return (
     <section>
@@ -28,6 +32,13 @@ export default async function Home() {
         {activites.map((activite) => (
           <CarteActivite key={activite.id} activite={activite} />
         ))}
+      </div>
+      <div className="mx-auto mb-[50px] flex max-w-[1140px] flex-col items-center">
+        <hr className="border-secondary mb-[30px] w-[80%] border-t" />
+        <h2 className="text-primary mb-[30px] font-light">
+          Foire aux questions
+        </h2>
+        <Accordeon faqs={faqs} />
       </div>
     </section>
   );
